d48ffd33b7ea49d616f0acb106d49c21e73fff3a
dupel <dupeljan@gmail.com>
2020-05-27 13:05:47 +0300

    Add bins

--- statisticEconom/plotting.py
+++ statisticEconom/plotting.py
@@ -11,35 +11,50 @@ table = list(csv.reader(open(INPUT,'r',newline='')))
 range_ = table
 range_.sort(key= lambda x : int(x[1])) 
 
+ 
 rangeSample = [int(x[1]) for x in range_]
-counter = Counter(rangeSample)
-x = list([0])
-y = list([0])
-for key in sorted(counter.keys()):
-	x.append(key)
-	y.append(y[-1] + counter[key])
-x = x[1:]
-y = y[1:]
-
-plt.step(x, y)
-plt.title("Эмпирическая функция расперделения")
-plt.show()
 
 len_ = int(max(rangeSample)) - int(min(rangeSample))
 min_ = int(min(rangeSample))
 max_ = int(max(rangeSample))
+h = int(np.round((max_ - min_) / ( 1 + 3.28*np.log(len_)) ))
+bins = np.round(len_ / h)
+print(bins)
+counter = Counter(rangeSample)
+xAccum = list([0])
+yAccum = list([0])
 
+x = list()
+y = list()
+for i in range(min_,max_+1,h):
+	x.append(i)
+	y.append(sum([counter.get(j,0) for j in range(i,i+h)]))
+	xAccum.append(i)
+	yAccum.append(yAccum[-1] + y[-1])
+	print(yAccum[-1])
+
+xAccum = xAccum[1:]
+yAccum = yAccum[1:]
+
+print(yAccum)
+plt.step(xAccum, yAccum)
+plt.title("Эмпирическая функция расперделения")
+plt.show()
+
+
+'''
 x = list()
 y = list()
 for i in range(min_,max_+1):
 	x.append(i)
 	y.append(counter.get(i,0))
+	'''
 '''	
 for key in sorted(counter.keys()):
 	x.append(int(key))
 	y.append(counter[key])
 '''
-plt.bar(x,y,width=1)
+plt.bar(x,y,width=h)
 plt.xticks(x)
 plt.yticks(np.arange(min(y),max(y)+1,1.0))
 plt.grid(axis='y')
